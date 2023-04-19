0cd68b371c62bde16b48235c75599064edf1458b
elianisvelascov <125934156+FabricioJimenezCampo@users.noreply.github.com>
2023-04-19 11:35:36 -0500

    trabajo1

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from store.models import Customer, product
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(product)
--- store/views.py
+++ store/views.py
@@ -1,13 +1,25 @@
+from itertools import product
+from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import product
+import datetime
 
 # Create your views here.
 
 def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+    products=product.objects.all()
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
 def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
+def get(request):
+    busqueda=request.GET.get("busqueda")
+    result=product.objects.filter(name=busqueda)
+    mensaje="EL RESULTADO ES: %s " 
+    return HttpResponse(mensaje)
+    
